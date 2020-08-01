000e98953fa7b4d97b4e930a8e48987cdd5f96f2
Anton Tutoveanu <anton.tutoveanu@gmail.com>
2020-08-01 12:53:19 +1000

    replaced frontend with my-app

--- frontend/src/components/Dashboard.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Landing = () => (
-    <h2>DASH</h2>
-);
-  
-export default Landing;
--- frontend/src/components/PrivPol.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Landing = () => (
-    <h2>PP</h2>
-);
-  
-export default Landing;

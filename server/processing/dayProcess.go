30d9617e7387ed5cfd5ce216a4b6ac050f469671
jn432 <jn432@uowmail.edu.au>
2020-08-12 22:47:35 +1000

    moved binary search to its own function

--- server/processing/dayProcess.go
+++ server/processing/dayProcess.go
@@ -50,22 +50,124 @@ func authenticate(email string, db *gorm.DB) *gmail.Service {
 	return srv
 }
 
+func binarySearchEmail(endTime int64, srv *gmail.Service) (emailIndex int, messages *gmail.ListMessagesResponse) {
+	//retrieve 128 email IDs. The maximum is 500, can be changed if needed
+	//128 is a nice number for binary search
+	messages, err := srv.Users.Messages.List("me").MaxResults(128).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve messages: %v", err)
+	}
+
+	//get the newest email in the list, only get minimal data since we only want the time received for now
+	emailIndex = -1
+	//loop until we run out of emails or we find the index
+	for {
+		mail, err := srv.Users.Messages.Get("me", messages.Messages[0].Id).Format("minimal").Do()
+		if err != nil {
+			log.Fatalf("Unable to retrieve message: %v", err)
+		}
+
+		//if endTime is before the first email, then check the last email
+		if endTime < mail.InternalDate {
+			//get the last email in the list
+			mail, err = srv.Users.Messages.Get("me", messages.Messages[len(messages.Messages)-1].Id).Format("minimal").Do()
+			if err != nil {
+				log.Fatalf("Unable to retrieve message: %v", err)
+			}
+			//if the last email in the list is after the given end time, then load the next 128 emails
+			if endTime < mail.InternalDate {
+				messages, err = srv.Users.Messages.List("me").PageToken(messages.NextPageToken).MaxResults(128).Do()
+			} else {
+				//otherwise newest message within date range must be between first and last email in the list, binary search
+				begin := 0
+				end := len(messages.Messages) - 1
+				middle := (begin + end) / 2
+				fmt.Println("B: ", begin, "E: ", end, "M: ", middle)
+				for {
+					//get the middle email
+					mail, err = srv.Users.Messages.Get("me", messages.Messages[middle].Id).Format("minimal").Do()
+					if err != nil {
+						log.Fatalf("Unable to retrieve message: %v", err)
+					}
+					fmt.Println("email retrieved")
+					fmt.Println("target: ", endTime, "current: ", mail.InternalDate)
+					//found an email at that exact time, found the latest email, break
+					if endTime == mail.InternalDate || begin == middle {
+						fmt.Println("found")
+						emailIndex = middle
+						break
+					} else if endTime > mail.InternalDate {
+						//end time is between start and middle
+						end = middle
+					} else {
+						//end time is between middle and end
+						begin = middle
+					}
+					middle = (begin + end) / 2
+					fmt.Println("B: ", begin, "E: ", end, "M: ", middle)
+				}
+
+			}
+		} else {
+			//found the email, was first one on the list, break the loop
+			emailIndex = 0
+			break
+		}
+		if messages.NextPageToken == "" || emailIndex >= 0 {
+			break
+		}
+	}
+
+	return emailIndex, messages
+
+}
+
 //ProcessDailyMail takes an email and the database that contains their token, connects
 //to gmail, and the previous days email. This should be run every night.
 func ProcessDailyMail(email string, db *gorm.DB) {
 	srv := authenticate(email, db)
-	//download 100 emails, client said some people will receive 100 emails/day, can be changed
-	messages, err := srv.Users.Messages.List("me").MaxResults(100).Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve messages: %v", err)
-	}
-	//get midnight of the previous day
 	today := time.Now()
 	year, month, day := today.Date()
 	//11:59:59PM of the previous day
 	endTime := time.Date(year, month, day, 0, 0, -1, 0, today.Location()).Unix() * 1000
 	//12:00:00AM of the previous day
 	beginTime := time.Date(year, month, day-1, 0, 0, 0, 0, today.Location()).Unix() * 1000
+
+	//find the most recent mail before endTime
+	emailIndex, messages := binarySearchEmail(endTime, srv)
+
+	emailCount := 0
+
+	//get emails, read horizontally, remember they will be from newest to oldest
+	for {
+		if emailIndex < len(messages.Messages) {
+			//load the next email
+			mail, err := srv.Users.Messages.Get("me", messages.Messages[emailIndex].Id).Format("metadata").Do()
+			if err != nil {
+				log.Fatalf("Unable to retrieve message: %v", err)
+			}
+			//if loaded email was sent before begin time, break the loop,the email is not the correct days
+			if mail.InternalDate < beginTime {
+				break
+			}
+			//do stats stuff here
+			emailCount++
+			fmt.Println(mail.Snippet)
+			emailIndex++
+		} else {
+			if messages.NextPageToken != "" {
+				//there are more emails, load the next 128 and reset the index
+				messages, err := srv.Users.Messages.List("me").PageToken(messages.NextPageToken).MaxResults(128).Do()
+				if err != nil {
+					log.Fatalf("Unable to retrieve messages: %v", err)
+				}
+				emailIndex = 0
+			} else {
+				//end of emails, break
+				break
+			}
+		}
+	}
 }
 
 // Takes the users email service and date range, downloads emails in that range and stores data into
@@ -82,74 +184,6 @@ func getMailDataRange(srv *gmail.Service, beginTime int64, endTime int64) {
 	beginTime = endTime - 7*24*60*60*1000
 
 	//END OF DUMMY CODE
-
-	//retrieve 500 email IDs. The maximum is 500, can be changed if needed
-	messages, err := srv.Users.Messages.List("me").MaxResults(500).Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve messages: %v", err)
-	}
-
-	//check if newest email within date range can exist within the 500 emails
-
-	//get the newest email in the list, only get minimal data since we only want the time received for now
-	var endID int = -1
-	//loop until we run out of emails or we find the endID
-	for {
-		mail, err := srv.Users.Messages.Get("me", messages.Messages[0].Id).Format("minimal").Do()
-		if err != nil {
-			log.Fatalf("Unable to retrieve message: %v", err)
-		}
-
-		//if last day in range is before the first email, then check the last email
-		if endTime < mail.InternalDate {
-			//get the last email in the list
-			mail, err = srv.Users.Messages.Get("me", messages.Messages[len(messages.Messages)-1].Id).Format("minimal").Do()
-			if err != nil {
-				log.Fatalf("Unable to retrieve message: %v", err)
-			}
-			//if the last email in the list is after the given end time, then load the next 500 emails
-			if endTime < mail.InternalDate {
-				messages, err = srv.Users.Messages.List("me").PageToken(messages.NextPageToken).MaxResults(500).Do()
-			} else {
-				//newest message within date range must be between first and last email in the list, binary search
-				begin := 0
-				end := len(messages.Messages) - 1
-				middle := (begin + end) / 2
-				fmt.Println("B: ", begin, "E: ", end, "M: ", middle)
-				for {
-					//get the middle email
-					mail, err = srv.Users.Messages.Get("me", messages.Messages[middle].Id).Format("minimal").Do()
-					if err != nil {
-						log.Fatalf("Unable to retrieve message: %v", err)
-					}
-					fmt.Println("email retrieved")
-					fmt.Println("target: ", endTime, "current: ", mail.InternalDate)
-					//found an email at that exact time, found the latest email, break
-					if endTime == mail.InternalDate || begin == middle {
-						fmt.Println("found")
-						endID = middle
-						break
-					} else if endTime > mail.InternalDate {
-						//end time is between start and middle
-						end = middle
-					} else {
-						//end time is between middle and end
-						begin = middle
-					}
-					middle = (begin + end) / 2
-					fmt.Println("B: ", begin, "E: ", end, "M: ", middle)
-				}
-
-			}
-		} else {
-			//found the email, was first one on the list, break the loop
-			endID = 0
-			break
-		}
-		if messages.NextPageToken == "" || endID >= 0 {
-			break
-		}
-	}
 
 	//code for when there are no emails
 	if endID < 0 {

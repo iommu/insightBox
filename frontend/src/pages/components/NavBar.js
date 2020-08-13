b0d30ace6c07bf7deafcf9b35de868d0e2310de7
Alex Carter <acarter010204@gmail.com>
2020-08-13 21:33:04 +1000

    added logout

--- frontend/src/pages/components/NavBar.js
+++ frontend/src/pages/components/NavBar.js
@@ -3,52 +3,63 @@ import {
   Flex, Button, Box, Text
 } from 'rebass';
 import {
-  Switch, Route, Link
+  Switch, Route, Link, withRouter
 } from "react-router-dom";
 import AuthButton from './Oauth';
 import { LogOut } from 'react-feather';
 import logo from '../../images/logo.png';
 
-
-function TopBar() {
-  return (
-    <Flex
-      px={2}
-      color='black'
-      bg='white'
-      alignItems='center'
-      sx={{borderBottom: '1px solid #bbb', boxShadow: '0px 0px 4px 1px #bbb'}}>
-      <img src={logo} alt="insightBox" width="75" />
-      <Text fontWeight='bold'>insightBox</Text>
-      <Switch>
-        <Route path="/signin"></Route>
-        <Route path="/dashboard">
-          <Box mx='auto' />
-          <button>
-            <LogOut />
-          </button>
-        </Route>
-        <Route path="/">
-          <Box mx='auto'/>
-          <Box>
-            <Link to="/" style={{color: '#333'}}>Home</Link>
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            <Link to="/about" style={{color: '#333'}}>About</Link>
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            <Link to="/privacypolicy" style={{color: '#333'}}>Privacy Policy</Link>
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            <Link to="/termsofuse" style={{color: '#333'}}>Terms Of Use</Link>
-          </Box>
-          <Box mx='auto' />
-          <AuthButton>
-          <Button sx={{backgroundColor:'white', color:'#c54339', border: '1px solid #c54339'}}>Sign Up</Button>
-          &nbsp;&nbsp;
-          <Button sx={{backgroundColor:'#c54339', color: 'white', border: '1px solid #c54339'}}>Login</Button>
-          </AuthButton>
-        </Route>
-      </Switch>
-    </Flex>
-  )
+class TopBar extends React.Component {
+  render() {
+    function LogOutFunc(that) {
+      localStorage.removeItem("token");
+      that.props.history.push("/");
+    }
+    return (
+      <Flex
+        px={2}
+        color='black'
+        bg='white'
+        alignItems='center'
+        sx={{ borderBottom: '1px solid #bbb', boxShadow: '0px 0px 4px 1px #bbb' }}>
+        <Link to="/">
+          <Flex
+            color='black'
+            alignItems='center'>
+            <img src={logo} alt="insightBox" width="75" />
+            <Text fontWeight='bold'>insightBox</Text>
+          </Flex>
+        </Link>
+        <Switch>
+          <Route path="/signin"></Route>
+          <Route path="/dashboard">
+            <Box mx='auto' />
+            <button onClick={() => { LogOutFunc(this) }}>
+              <LogOut />
+            </button>
+          </Route>
+          <Route path="/">
+            <Box mx='auto' />
+            <Box>
+              <Link to="/" style={{ color: '#333' }}>Home</Link>
+              &nbsp;&nbsp;|&nbsp;&nbsp;
+              <Link to="/about" style={{ color: '#333' }}>About</Link>
+              &nbsp;&nbsp;|&nbsp;&nbsp;
+              <Link to="/privacypolicy" style={{ color: '#333' }}>Privacy Policy</Link>
+              &nbsp;&nbsp;|&nbsp;&nbsp;
+              <Link to="/termsofuse" style={{ color: '#333' }}>Terms Of Use</Link>
+            </Box>
+            <Box mx='auto' />
+            <AuthButton>
+              <Button sx={{ backgroundColor: 'white', color: '#c54339', border: '1px solid #c54339' }}>Sign Up</Button>
+            &nbsp;&nbsp;
+            <Button sx={{ backgroundColor: '#c54339', color: 'white', border: '1px solid #c54339' }}>Login</Button>
+            </AuthButton>
+          </Route>
+        </Switch>
+      </Flex>
+    )
+  }
 }
 
-export default TopBar;
+export default withRouter(TopBar);

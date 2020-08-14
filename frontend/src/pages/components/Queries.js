6168ffeca7f332c649812beefafe17987d7dc751
Alex Carter <acarter010204@gmail.com>
2020-08-14 17:52:34 +1000

    day query test

--- frontend/src/pages/components/Queries.js
+++ frontend/src/pages/components/Queries.js
@@ -29,3 +29,33 @@ export const User = () => {
     <p> Welcome {data.user.id}</p>
   );
 }
+
+export const User = () => {
+  const [result, getDayData] = useQuery({
+    query: `
+    query {
+      data(start:"2020-08-07 00:00:00", end:"2020-08-07 00:00:00") {
+        id,
+        date
+      }
+    }`
+  });
+
+  console.log(result);
+
+  const { data, fetching, error } = result;
+
+  if (fetching) return (
+    <p> Loading user data </p>
+  );
+
+  if (error) {
+    return (
+      <p> Error getting user data </p>
+    );
+  };
+
+  return (
+    <p> Welcome</p>
+  );
+}

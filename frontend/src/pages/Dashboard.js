de7ca682ac84bc32f3bca361590a6327036b415e
Alex Carter <acarter010204@gmail.com>
2020-09-06 12:32:14 +1000

    Revert "added dashboard redirect"
    This reverts commit 664945b5dfcb26d345dab1b398032058fbf62847.

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { User, Day } from './components/Queries';
-import { withRouter } from 'react-router-dom';
 
 // import graphql and create client
 import { createClient, Provider } from 'urql';
@@ -19,147 +18,137 @@ const client = createClient({
   },
 });
 
-class Dashboard extends React.Component {
-  constructor(props) {
-    super(props);
-    if (!localStorage.getItem("token")) {
-      console.log("error: no login token, redirecting");
-      this.props.history.push("/");
-    }
-  }
+function Dashboard() {
+  return (
+    <Provider value={client}>
+      <div>
+        <div class="dashboard-content">
+          <Flex flexWrap='wrap' m={20} >
+            <Box 
+              sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
+              width={[ 1, 47.5/100 ]}
+              m={3}
+              p={3}>
+              Reflex
+            </Box>
+            <Box 
+              sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
+              width={[ 1, 47.5/100 ]}
+              m={3}
+              p={3}>
+              Box
+            </Box>
 
-  render() {
-    return (
-      <Provider value={client}>
-        <div>
-          <div class="dashboard-content">
-            <Flex flexWrap='wrap' m={20} >
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 1, 47.5/100 ]}
-                m={3}
-                p={3}>
-                Reflex
-              </Box>
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 1, 47.5/100 ]}
-                m={3}
-                p={3}>
-                Box
-              </Box>
-  
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 2, 31/100 ]}
-                m={3}
-                p={3}>
-                Reflex
-              </Box>
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 2, 31/100 ]}
-                m={3}
-                p={3}>
-                Box
-              </Box>
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 2, 31/100 ]}
-                m={3}
-                p={3}>
-                Box
-              </Box>
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 1, 47/100 ]}
-                m={3}
-                p={3}>
-                Reflex
-              </Box>
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 1, 47/100 ]}
-                m={3}
-                p={3}>
-                Box
-              </Box>
-  
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 2, 31/100 ]}
-                m={3}
-                p={3}>
-                Reflex
-              </Box>
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 2, 31/100 ]}
-                m={3}
-                p={3}>
-                Box
-              </Box>
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 2, 31/100 ]}
-                m={3}
-                p={3}>
-                Box
-              </Box>
-  
-  
-              <Box width={4/10} m={10} px={2} py={2} sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
-                <Text p={1} color='black' bg='primary'>
-                Graph
-                </Text>
-              </Box>
-              <Box width={4/10} m={10} px={2} py={2} sx={{textAlign: 'center',borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
-                <Text p={1} color='black' bg='primary'>
-                <User />
-                </Text>
-              </Box>
-              <Box width={1/3} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
-                <Text p={1} color='black' bg='primary'>
-                <Day />
-                </Text>
-              </Box>
-  
-              <Box width={1/3} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
-                <Text p={1} color='black' bg='primary'>
-                Graph
-                </Text>
-              </Box>
-              <Box width={1/3} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
-                <Text p={1} color='black' bg='primary'>
-                <User />
-                </Text>
-              </Box>
-              <Box width={1/4} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
-                <Text p={1} color='black' bg='primary'>
-                <Day />
-                </Text>
-              </Box>
-              <Box width={1/4} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
-                <Text p={1} color='black' bg='primary'>
-                Data
-                </Text>
-              </Box>
-              <Box width={1/4} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
-                <Text p={1} color='black' bg='primary'>
-                Data
-                </Text>
-              </Box>
-              <Box width={1/4} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
-                <Text p={1} color='black' bg='primary'>
-                Data
-                </Text>
-              </Box>
-            </Flex>
-          </div>
+            <Box 
+              sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
+              width={[ 2, 31/100 ]}
+              m={3}
+              p={3}>
+              Reflex
+            </Box>
+            <Box 
+              sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
+              width={[ 2, 31/100 ]}
+              m={3}
+              p={3}>
+              Box
+            </Box>
+            <Box 
+              sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
+              width={[ 2, 31/100 ]}
+              m={3}
+              p={3}>
+              Box
+            </Box>
+            <Box 
+              sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
+              width={[ 1, 47/100 ]}
+              m={3}
+              p={3}>
+              Reflex
+            </Box>
+            <Box 
+              sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
+              width={[ 1, 47/100 ]}
+              m={3}
+              p={3}>
+              Box
+            </Box>
+
+            <Box 
+              sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
+              width={[ 2, 31/100 ]}
+              m={3}
+              p={3}>
+              Reflex
+            </Box>
+            <Box 
+              sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
+              width={[ 2, 31/100 ]}
+              m={3}
+              p={3}>
+              Box
+            </Box>
+            <Box 
+              sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
+              width={[ 2, 31/100 ]}
+              m={3}
+              p={3}>
+              Box
+            </Box>
+
+
+            <Box width={4/10} m={10} px={2} py={2} sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
+              <Text p={1} color='black' bg='primary'>
+              Graph
+              </Text>
+            </Box>
+            <Box width={4/10} m={10} px={2} py={2} sx={{textAlign: 'center',borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
+              <Text p={1} color='black' bg='primary'>
+              <User />
+              </Text>
+            </Box>
+            <Box width={1/3} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
+              <Text p={1} color='black' bg='primary'>
+              <Day />
+              </Text>
+            </Box>
+
+            <Box width={1/3} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
+              <Text p={1} color='black' bg='primary'>
+              Graph
+              </Text>
+            </Box>
+            <Box width={1/3} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
+              <Text p={1} color='black' bg='primary'>
+              <User />
+              </Text>
+            </Box>
+            <Box width={1/4} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
+              <Text p={1} color='black' bg='primary'>
+              <Day />
+              </Text>
+            </Box>
+            <Box width={1/4} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
+              <Text p={1} color='black' bg='primary'>
+              Data
+              </Text>
+            </Box>
+            <Box width={1/4} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
+              <Text p={1} color='black' bg='primary'>
+              Data
+              </Text>
+            </Box>
+            <Box width={1/4} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
+              <Text p={1} color='black' bg='primary'>
+              Data
+              </Text>
+            </Box>
+          </Flex>
         </div>
-      </Provider>
-    );
-  };
+      </div>
+    </Provider>
+  );
 }
 
-export default withRouter(Dashboard);
+export default Dashboard;

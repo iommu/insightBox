d2e070c86c09f55763128fa19fa9cc62382ee81b
Alex Carter <acarter010204@gmail.com>
2020-09-06 20:22:59 +1000

    Rebass -> HTML

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -33,128 +33,52 @@ class Dashboard extends React.Component {
       <Provider value={client}>
         <div>
           <div class="dashboard-content">
-            <Flex flexWrap='wrap' m={20} >
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 1, 47.5/100 ]}
-                m={3}
-                p={3}>
-                Reflex
-              </Box>
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 1, 47.5/100 ]}
-                m={3}
-                p={3}>
-                Box
-              </Box>
+            <div class="stat-container">
+              <User />
+            </div>
+            <div class="stat-container">
+              <Day />
+            </div>
+            <div class="stat-container">
+              Graph
+            </div>
+            <div class="stat-container">
+              Graph
+            </div>
+            <div class="stat-container">
+              Graph
+            </div>
+            <div class="stat-container">
+              Graph
+            </div>
+            <div class="stat-container">
+              Graph
+            </div>
+            <div class="stat-container">
+              Graph
+            </div>
+            <div class="stat-container">
+              Graph
+            </div>
+            <div class="stat-container">
+              Graph
+            </div>
+            <div class="stat-container">
+              Graph
+            </div>
+            <div class="stat-container">
+              Graph
+            </div>
+            <div class="stat-container">
+              Graph
+            </div>
+            <div class="stat-container">
+              Graph
+            </div>
+            <div class="stat-container">
+              Graph
+            </div>
   
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 2, 31/100 ]}
-                m={3}
-                p={3}>
-                Reflex
-              </Box>
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 2, 31/100 ]}
-                m={3}
-                p={3}>
-                Box
-              </Box>
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 2, 31/100 ]}
-                m={3}
-                p={3}>
-                Box
-              </Box>
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 1, 47/100 ]}
-                m={3}
-                p={3}>
-                Reflex
-              </Box>
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 1, 47/100 ]}
-                m={3}
-                p={3}>
-                Box
-              </Box>
-  
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 2, 31/100 ]}
-                m={3}
-                p={3}>
-                Reflex
-              </Box>
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 2, 31/100 ]}
-                m={3}
-                p={3}>
-                Box
-              </Box>
-              <Box 
-                sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}
-                width={[ 2, 31/100 ]}
-                m={3}
-                p={3}>
-                Box
-              </Box>
-  
-  
-              <Box width={4/10} m={10} px={2} py={2} sx={{textAlign: 'center', borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
-                <Text p={1} color='black' bg='primary'>
-                Graph
-                </Text>
-              </Box>
-              <Box width={4/10} m={10} px={2} py={2} sx={{textAlign: 'center',borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
-                <Text p={1} color='black' bg='primary'>
-                <User />
-                </Text>
-              </Box>
-              <Box width={1/3} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
-                <Text p={1} color='black' bg='primary'>
-                <Day />
-                </Text>
-              </Box>
-  
-              <Box width={1/3} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
-                <Text p={1} color='black' bg='primary'>
-                Graph
-                </Text>
-              </Box>
-              <Box width={1/3} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
-                <Text p={1} color='black' bg='primary'>
-                <User />
-                </Text>
-              </Box>
-              <Box width={1/4} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
-                <Text p={1} color='black' bg='primary'>
-                <Day />
-                </Text>
-              </Box>
-              <Box width={1/4} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
-                <Text p={1} color='black' bg='primary'>
-                Data
-                </Text>
-              </Box>
-              <Box width={1/4} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
-                <Text p={1} color='black' bg='primary'>
-                Data
-                </Text>
-              </Box>
-              <Box width={1/4} px={2} py={2} sx={{borderWidth: '1px',borderStyle: 'solid',borderColor: 'black', backgroundColor: 'white'}}>
-                <Text p={1} color='black' bg='primary'>
-                Data
-                </Text>
-              </Box>
-            </Flex>
           </div>
         </div>
       </Provider>

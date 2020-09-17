bda47d24743e1b8055e2bc0f0e80a67eaaef5135
Anton Tutoveanu <anton.tutoveanu@gmail.com>
2020-09-17 20:39:01 +1000

    rechart

---
+++ frontend/src/pages/components/Graph1.js
@@ -0,0 +1,20 @@
+
+
+
+import React from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+
+const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, {name: 'Page B', uv: 500, pv: 3400, amt: 3000}, {name: 'Page C', uv: 600, pv: 4000, amt: 4000}];
+
+function Graph1() {
+    return (
+        <LineChart width={600} height={300} data={data}>
+            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="name" />
+            <YAxis />
+        </LineChart>
+    )
+  }
+
+export default Graph1;

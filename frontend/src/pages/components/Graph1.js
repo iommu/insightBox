4f28a73b66e7181479ff473d9658e95a36fe1c7e
Anton Tutoveanu <anton.tutoveanu@gmail.com>
2020-09-17 20:58:59 +1000

    rechart

--- frontend/src/pages/components/Graph1.js
+++ frontend/src/pages/components/Graph1.js
@@ -8,7 +8,7 @@ const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, {name: 'Page B', u
 
 function Graph1() {
     return (
-        <LineChart width={600} height={300} data={data}>
+        <LineChart width={500} height={200} data={data}>
             <Line type="monotone" dataKey="uv" stroke="#8884d8" />
             <CartesianGrid stroke="#ccc" />
             <XAxis dataKey="name" />

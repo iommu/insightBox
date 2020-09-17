1fa4ccce23ca6570019f90c7a1197ccaa0b5e974
Anton Tutoveanu <anton.tutoveanu@gmail.com>
2020-09-18 09:29:47 +1000

    rechart

--- frontend/src/pages/components/Queries.js
+++ frontend/src/pages/components/Queries.js
@@ -66,6 +66,23 @@ export const Day = () => {
     </table>
   );
 }
+/*
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
+const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, ...];
+
+const renderBarChart = (
+  <BarChart width={600} height={200} data={data}>
+    <XAxis dataKey="name" stroke="#8884d8" />
+    <YAxis />
+    <Tooltip />
+    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+    <Bar dataKey="uv" fill="#8884d8" barSize={30} />
+  </BarChart>
+);
+*/
+
+
+
 
 export const ID = () => {
   const [result, getDayData] = useQuery({

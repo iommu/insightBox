db4912839988e19f356c1624491261f7898b4f2f
Anton Tutoveanu <anton.tutoveanu@gmail.com>
2020-09-20 09:51:40 +1000

    frontend

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-
+import info from '../images/info.png';
 import { Graph1 } from './components/Graph1';
 import { Graph2 } from './components/Graph2';
 import { Graph3 } from './components/Graph3';
@@ -21,50 +21,94 @@ class Dashboard extends React.Component {
           <div className="dashboard-content">
             <div className="stat-container">
               <Graph1 />
+              <div className="info">
+                <img className="info-img" src={info} alt="info" />
+              </div>
             </div>
             <div className="stat-container">
               <Graph2 />
+              <div className="info">
+                <img className="info-img" src={info} alt="info" />
+              </div>
             </div>
             <div className="stat-container">
               <Graph3 />
+              <div className="info">
+                <img className="info-img" src={info} alt="info" />
+              </div>
             </div>
             <div className="stat-container">
               <Graph4 />
+              <div className="info">
+                <img className="info-img" src={info} alt="info" />
+              </div>
             </div>
             <div className="stat-container">
               Graph
+              <div className="info">
+                <img className="info-img" src={info} alt="info" />
+              </div>
             </div>
             <div className="stat-container">
               Graph
+              <div className="info">
+                <img className="info-img" src={info} alt="info"/>
+              </div>
             </div>
             <div className="stat-container">
               Graph
+              <div className="info">
+                <img className="info-img" src={info} alt="info" />
+              </div>
             </div>
             <div className="stat-container">
               Graph
+              <div className="info">
+                <img className="info-img" src={info} alt="info" />
+              </div>
             </div>
             <div className="stat-container">
               Graph
+              <div className="info">
+                <img className="info-img" src={info} alt="info" />
+              </div>
             </div>
             <div className="stat-container">
               Graph
+              <div className="info">
+                <img className="info-img" src={info} alt="info" />
+              </div>
             </div>
             <div className="stat-container">
               Graph
+              <div className="info">
+                <img className="info-img" src={info} alt="info" />
+              </div>
             </div>
             <div className="stat-container">
               Graph
+              <div className="info">
+                <img className="info-img" src={info} alt="info" />
+              </div>
             </div>
             <div className="stat-container">
               Graph
+              <div className="info">
+                <img className="info-img" src={info} alt="info" />
+              </div>
             </div>
             <div className="stat-container">
               Graph
+              <div className="info">
+                <img className="info-img" src={info} alt="info" />
+              </div>
             </div>
             <div className="stat-container">
               Graph
+              <div className="info">
+                <img className="info-img" src={info} alt="info" />
+              </div>
             </div>
-  
           </div>
         </div>
     );

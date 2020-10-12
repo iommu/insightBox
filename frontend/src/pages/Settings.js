be636045ef1bb046b5365aca645fe521bfaf1976
Alex Carter <acarter010204@gmail.com>
2020-10-12 18:13:14 +1100

    Finished settings page

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,18 +1,46 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import { useMutation } from 'urql';
 
-function Settings() {
-  const history = useHistory();
+const DeleteAccountMutation = `
+mutation DelMut($email: String!) {
+  deleteAccount(email: $email)
+}
+`;
 
-  function delAcc() {
-    console.log("//TODO: delete account");
-    // localStorage.removeItem("token");
-    // localStorage.removeItem("old_code");
-    // send query
-    history.push("/");
-  }
+const Settings = props => {
 
-  return <button onClick={delAcc}>Delete Account</button>;
+  const [email, setEmail] = React.useState('');
+
+  const [result, executeMutation] = useMutation(DeleteAccountMutation);
+
+  const deleteAccount = React.useCallback(() => {
+    executeMutation({ email }).then((result) => {
+      if(result.data){
+        if(result.data.deleteAccount==0){
+          console.log("account deletion sucessful! :(");
+          //localStorage.removeItem("token");
+          //localStorage.removeItem("old_code");
+          //props.history.push("/");
+        } else {
+          console.log("account deletion failed! :(");  
+          document.getElementById("errorText").innerHTML = "Error deleting account, check that the typed email is the email address of this account";
+        }
+      }
+    });
+  }, [email, executeMutation]);
+
+  return(
+    <div>
+      <label for="fname">Please confirm your email address:</label><br/>
+      <input type="text"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      /><br/>
+      <button disabled={result.fetching} onClick={deleteAccount}>deleteAccount</button>
+      <p id="errorText"></p>
+    </div>
+  );
 }
 
-export default Settings;
+export default withRouter(Settings);

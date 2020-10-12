28f0da4ae07216873715e8aa7488f150cbbdd327
Alex Carter <acarter010204@gmail.com>
2020-10-12 19:56:12 +1100

    Compressed sidebar + calendar into just sidebar

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import info from '../images/info.png';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import info from "../images/info.png";
+import { Graph1 } from "./components/Graph1";
+import { Graph2 } from "./components/Graph2";
+import { Graph3 } from "./components/Graph3";
+import { Graph4 } from "./components/Graph4";
+import { Graph5 } from "./components/Graph5";
+import { Graph6 } from "./components/Graph6";
+import { Graph7 } from "./components/Graph7";
+import { Graph8 } from "./components/Graph8";
 import SideBar from "./components/SideBar";
-import { Graph1 } from './components/Graph1';
-import { Graph2 } from './components/Graph2';
-import { Graph3 } from './components/Graph3';
-import { Graph4 } from './components/Graph4';
-import { Graph5 } from './components/Graph5';
-import { Graph6 } from './components/Graph6';
-import { Graph7 } from './components/Graph7';
-import { Graph8 } from './components/Graph8';
 
 class Dashboard extends React.Component {
   constructor(props) {
@@ -18,65 +18,77 @@ class Dashboard extends React.Component {
       console.log("error: no login token, redirecting");
       this.props.history.push("/");
     }
+    this.state = {
+      startDate: new Date().toISOString(),
+      endDate: new Date().toISOString()
+    };
+  }
+
+  updateDates = (startDate, endDate) => {
+    this.setState({
+      startDate: startDate,
+      endDate: endDate
+    });
   }
 
   render() {
     return (
-        <div id="dash-container">
-          <SideBar/>
-          <div className="dashboard-content" style={{marginLeft:"300px"}}>
-            <div className="stat-container">
-              <Graph1 />
-              <div className="info">
-                <img className="info-img" src={info} alt="info" />
-              </div>
+      <div id="dash-container">
+        <p>Date range is from ${this.state.startDate} to ${this.state.endDate}</p>
+        <SideBar updateDates={this.updateDates}/>
+        <div className="dashboard-content" style={{ marginLeft: "300px" }}>
+          <div className="stat-container">
+            <Graph1 />
+            <div className="info">
+              <img className="info-img" src={info} alt="info" />
             </div>
-            <div className="stat-container">
-              <Graph2 />
-              <div className="info">
-                <img className="info-img" src={info} alt="info" />
-              </div>
+          </div>
+          <div className="stat-container">
+            <Graph2 />
+            <div className="info">
+              <img className="info-img" src={info} alt="info" />
             </div>
-            <div className="stat-container">
-              <Graph3 />
-              <div className="info">
-                <img className="info-img" src={info} alt="info" />
-              </div>
+          </div>
+          <div className="stat-container">
+            <Graph3 />
+            <div className="info">
+              <img className="info-img" src={info} alt="info" />
             </div>
-            <div className="stat-container">
-              <Graph4 />
-              <div className="info">
-                <img className="info-img" src={info} alt="info" />
-              </div>
+          </div>
+          <div className="stat-container">
+            <Graph4 />
+            <div className="info">
+              <img className="info-img" src={info} alt="info" />
             </div>
-            <div className="stat-container">
-              <Graph5 />
-              <div className="info">
-                <img className="info-img" src={info} alt="info" />
-              </div>
+          </div>
+          <div className="stat-container">
+            <Graph5 />
+            <div className="info">
+              <img className="info-img" src={info} alt="info" />
             </div>
-            <div className="stat-container">
-              <Graph6 />
-              <div className="info">
-                <img className="info-img" src={info} alt="info"/>
-              </div>
+          </div>
+          <div className="stat-container">
+            <Graph6 />
+            <div className="info">
+              <img className="info-img" src={info} alt="info" />
             </div>
-            <div className="stat-container">
-              <Graph7 />
-              <div className="info">
-                <img className="info-img" src={info} alt="info" />
-              </div>
+          </div>
+          <div className="stat-container">
+            <Graph7 />
+            <div className="info">
+              <img className="info-img" src={info} alt="info" />
             </div>
-            <div className="stat-container">
-              <Graph8 />
-              <div className="info">
-                <img className="info-img" src={info} alt="info" />
-              </div>
+          </div>
+          <div className="stat-container">
+            <Graph8 />
+            <div className="info">
+              <img className="info-img" src={info} alt="info" />
             </div>
           </div>
         </div>
+      </div>
     );
-  };
+  }
 }
 
 export default withRouter(Dashboard);

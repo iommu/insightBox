2a04367f234aa9d86df602056c322de23fc3d65a
samdahman <68273265+samdahman@users.noreply.github.com>
2020-10-17 18:36:02 +1100

    testin

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,4 +18,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+// test 4 sam
 serviceWorker.unregister();

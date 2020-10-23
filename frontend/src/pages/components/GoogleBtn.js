333530bf0b4730af9231a014441508462cd5d390
Anton Tutoveanu <anton.tutoveanu@gmail.com>
2020-10-23 11:30:47 +1100

    frontend

--- frontend/src/pages/components/GoogleBtn.js
+++ frontend/src/pages/components/GoogleBtn.js
@@ -5,7 +5,7 @@ import AuthButton from "./Oauth";
 export default () => (
   <AuthButton>
     <div id="gSignInWrapper">
-      <span className="label">Sign in with : </span>
+      <span className="label">Sign up with : </span>
       <button id="customBtn" className="customGPlusSignIn">
         <span className="icon"></span>
         <span className="buttonText">Google</span>

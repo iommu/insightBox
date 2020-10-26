7685574570d832b472b00684a9c529ad82e88876
Alex Carter <acarter010204@gmail.com>
2020-10-26 19:50:09 +1100

    updated cipher

--- frontend/src/pages/Cipher.js
+++ frontend/src/pages/Cipher.js
@@ -1,32 +1,38 @@
-import React from "react";
-import { useQuery } from "urql";
+import React, { useReducer } from "react";
+import { useQuery, useClient } from "urql";
 import { Graphtest } from "./components/Graphtest";
 import { GenerateKEM } from "./components/Crypto";
 
 export const Cipher = () => {
-    // generate a (c, ss) pair
-    var output = new Array(2);
-    output = GenerateKEM();
-    // save in localStorage
-    localStorage.c_tmp = output[0];
-    localStorage.ss_tmp = output[1];
-    // convert c to hex string
-    var hexStr = bytesToHexStr(output[0]);
-    console.log(hexStr);
-    // send hex string to server: setC()
-    const [result] = useQuery({
-        query:
-            `
+    const client = useClient();
+    if (localStorage.getItem("ss") == null) {
+        // generate a (c, ss) pair
+        var output = new Array(2);
+        output = GenerateKEM();
+        // save in localStorage
+        localStorage.c_tmp = output[0];
+        localStorage.ss_tmp = output[1];
+        // convert c to hex string
+        var hexStr = bytesToHexStr(output[0]);
+        console.log(hexStr);
+        // send hex string to server: setC()
+        client
+            .query(
+                `
             query {
               getCipher(cTmp:"` +
-            hexStr +
-            `")
-            }`,
-    });
-    const { fetching, error } = result;
-    // TODO add error handling
-    // result.data.getCipher
-    return <p>ehhhhh</p>;
+                    hexStr +
+                    `")
+            }`
+            )
+            .toPromise()
+            .then((result) => {
+                console.log(result);
+            });
+        // TODO add error handling
+        // result.data.getCipher
+    }
+    return (null);
 };
 
 function bytesToHexStr(c) {

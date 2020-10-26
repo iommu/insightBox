453e0d4fb0b0cbd9be1984ea6cca996f7056d07b
Alex Carter <acarter010204@gmail.com>
2020-10-27 09:28:26 +1100

    deleted old resolver code

--- server/graph/schema.resolvers.go
+++ server/graph/schema.resolvers.go
@@ -5,7 +5,6 @@ package graph
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"time"
 
@@ -85,16 +84,3 @@ func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *mutationResolver) SetC(ctx context.Context, c string) (int, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-func (r *queryResolver) GetSs(ctx context.Context) (string, error) {
-	panic(fmt.Errorf("not implemented"))
-}

05e4d5cf38cf1802d24df974b0e5cd552267287e
antontutoveanu <anton.tutoveanu@gmail.com>
2020-11-04 16:46:12 +1100

    front pages

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -13,18 +13,26 @@ return(
             <div class="homepage-content-about-text">
             <br/>
             <br/>
+            <h1>About</h1>
             <p>
-                insightBox is an email analytics service that connects to
-                a Gmail account and provides insights and statistics. The main
-                aim of the service is to provide users with information so that
-                they can take action upon the insights provided, and better
-                streamline their email habits to improve work and
-                scheduling habits.
+            <b>insightBox</b> is a web application that displays statistics and insights based on a user's Gmail account. It is aimed at working professionals who use email with the purpose to increase productivity and optimise their time spent in inbox.
+            </p>
+            <p>The application gets user data by using the OAuth 2.0 protocol along with the Google API.</p>
+            <br />
+            <p>Email metadata is downloaded and processed by the server to generate analytics. Metadata includes:
+            <div id="ul-about">
+                <ul>
+                    <li>Subject title</li>
+                    <li>Date/time</li>
+                    <li>Sender information</li>
+                    <li>Recipient information</li>
+                </ul>
+            </div>
             </p>
             <br />
-            <h2>End-to-end Post-Quantum Security</h2>
+            <h2>End-to-End Post-Quantum Security</h2>
             <p>
-                insightBox provides end-to-end encryption between our server and the end-user's device using CRYSTALS-KYBER and AES-256.
+            Privacy is important so sensitive data like subject title and sender/recipient information are encrypted. insightBox uses quantum-resistant, end-to-end encryption to send sensitive data from the server to the user using CRYSTALS-KYBER and AES-256.
             </p>
             <br/>
             <p>
